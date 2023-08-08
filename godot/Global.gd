d90f38f55ff1394e48cd1f7566ca67f80a3ed9f1
Cactus <115384394+Cactoidal@users.noreply.github.com>
2023-08-08 03:44:57 -0400

    Update Global.gd

--- godot/Global.gd
+++ godot/Global.gd
@@ -1,13 +1,15 @@
-extends Node
+extends Control
 
 var user_address
 var pilot
+
+var create_player_pilot = false
 
 var current_chain = get_chain_info("Fuji")
 var destination_chain = get_chain_info("Optimism")
 
 var chain_selector = 0
-var available_chains = ["Optimism", "Arbitrum", "Sepolia", "Mumbai"]
+var available_chains = ["Optimism", "Arbitrum", "Sepolia", "Mumbai", "Fuji"]
 
 var fuji_rpc = "https://avalanche-fuji-c-chain.publicnode.com"
 var mumbai_rpc = "https://rpc-mumbai.maticvigil.com"
@@ -16,11 +18,11 @@ var optimism_rpc = "https://optimism-goerli.publicnode.com"
 var arbitrum_rpc = "https://arbitrum-goerli.publicnode.com"
 
 var balance_selector = 0
-var fuji_balance = "0"
-var mumbai_balance = "0"
-var sepolia_balance = "0"
-var optimism_balance = "0"
-var arbitrum_balance = "0"
+var fuji_balance = 0
+var mumbai_balance = 0
+var sepolia_balance = 0
+var optimism_balance = 0
+var arbitrum_balance = 0
 
 var fuji_faucet = "https://core.app/en/tools/testnet-faucet/?subnet=c&token=c"
 var mumbai_faucet = "https://faucet.polygon.technology"
@@ -30,9 +32,9 @@ var arbitrum_faucet = "https://faucet.quicknode.com/arbitrum/goerli"
 
 var fuji_stardust = "0x091ec5F9c7d12DfCa9468f662e2f395Cb9656c75"
 var mumbai_stardust = "0xF0c3F3Ef23ACF07764e42342e85eE248A9bEd081"
-var sepolia_stardust
-var optimism_stardust
-var arbitrum_stardust
+var sepolia_stardust = "0x822D1FcC6544Ec59dce2A68A4c6507a3D099496b"
+var optimism_stardust = "0x78FE5243cf6143d98C8b5847c8E1b15Cf53D8338"
+var arbitrum_stardust = "0x78FE5243cf6143d98C8b5847c8E1b15Cf53D8338"
 
 var fuji_id = 43113
 var mumbai_id = 80001
@@ -46,11 +48,11 @@ var sepolia_selector = "DE41BA4FC9D91AD9"
 var optimism_selector = "24F9B897EF58A922"
 var arbitrum_selector = "54ABF9FB1AFEAF95"
 
-var fuji_flight_time = 5
-var optimism_flight_time = 25
-var arbitrum_flight_time = 25
-var mumbai_flight_time = 25
-var sepolia_flight_time = 25
+var fuji_flight_time = 3#300
+var optimism_flight_time = 3#1500
+var arbitrum_flight_time = 3#1500
+var mumbai_flight_time = 3#1500
+var sepolia_flight_time = 3#1500
 
 var fuji_logo = load("res://buttons/Avalanche.png")
 var arbitrum_logo = load("res://buttons/arbitrum.png")
@@ -59,19 +61,32 @@ var sepolia_logo = load("res://buttons/Ethereum.png")
 var mumbai_logo = load("res://buttons/Polygon.png")
 var chainlink_logo = load("res://buttons/chainlink.png")
 
+var fuji_color = [1.0, 0.0, 0.3]
+var mumbai_color = [0.48, 0.0, 0.9]
+var optimism_color = [1.0, 0.0, 0.1]
+var arbitrum_color = [0.18, 0.18, 0.68]
+var sepolia_color = [1.0, 1.0, 0.68]
+
 var launch_console
+var blockspace
+
+var entering_port_timer = -1
+var entering_port = false
+
+var start_in_warp = false
 
 func get_chain_info(var chain):
 	match chain:
-		"Fuji": return {"name": "Fuji", "rpc": fuji_rpc, "chain_id": fuji_id, "chain_selector": fuji_selector, "flight_time": fuji_flight_time, "logo": fuji_logo, "player_balance": fuji_balance, "stardust_contract": fuji_stardust}
-		"Optimism": return {"name": "Optimism", "rpc": optimism_rpc, "chain_id": optimism_id, "chain_selector": optimism_selector, "flight_time": optimism_flight_time, "logo": optimism_logo, "player_balance": optimism_balance, "stardust_contract": optimism_stardust}
-		"Arbitrum": return {"name": "Arbitrum", "rpc": arbitrum_rpc, "chain_id": arbitrum_id, "chain_selector": arbitrum_selector, "flight_time": arbitrum_flight_time, "logo": arbitrum_logo, "player_balance": arbitrum_balance, "stardust_contract": arbitrum_stardust}
-		"Mumbai": return {"name": "Mumbai", "rpc": mumbai_rpc, "chain_id": mumbai_id, "chain_selector": mumbai_selector, "flight_time": mumbai_flight_time, "logo": mumbai_logo, "player_balance": mumbai_balance, "stardust_contract": mumbai_stardust}
-		"Sepolia": return {"name": "Sepolia", "rpc": sepolia_rpc, "chain_id": sepolia_id, "chain_selector": sepolia_selector, "flight_time": sepolia_flight_time, "logo": sepolia_logo, "player_balance": sepolia_balance, "stardust_contract": sepolia_stardust}
+		"Fuji": return {"name": "Fuji", "rpc": fuji_rpc, "chain_id": fuji_id, "chain_selector": fuji_selector, "flight_time": fuji_flight_time, "logo": fuji_logo, "player_balance": fuji_balance, "stardust_contract": fuji_stardust, "color": fuji_color}
+		"Optimism": return {"name": "Optimism", "rpc": optimism_rpc, "chain_id": optimism_id, "chain_selector": optimism_selector, "flight_time": optimism_flight_time, "logo": optimism_logo, "player_balance": optimism_balance, "stardust_contract": optimism_stardust, "color": optimism_color}
+		"Arbitrum": return {"name": "Arbitrum", "rpc": arbitrum_rpc, "chain_id": arbitrum_id, "chain_selector": arbitrum_selector, "flight_time": arbitrum_flight_time, "logo": arbitrum_logo, "player_balance": arbitrum_balance, "stardust_contract": arbitrum_stardust, "color": arbitrum_color}
+		"Mumbai": return {"name": "Mumbai", "rpc": mumbai_rpc, "chain_id": mumbai_id, "chain_selector": mumbai_selector, "flight_time": mumbai_flight_time, "logo": mumbai_logo, "player_balance": mumbai_balance, "stardust_contract": mumbai_stardust, "color": mumbai_color}
+		"Sepolia": return {"name": "Sepolia", "rpc": sepolia_rpc, "chain_id": sepolia_id, "chain_selector": sepolia_selector, "flight_time": sepolia_flight_time, "logo": sepolia_logo, "player_balance": sepolia_balance, "stardust_contract": sepolia_stardust, "color": sepolia_color}
 
 var flight_timer = 0
 var in_flight = false
 func start_flight_timer(var flight_time):
+	blockspace.color_time = 3
 	launch_console.get_node("Timer").visible = true
 	launch_console.get_node("Timer").text = String(flight_timer)
 	flight_timer = flight_time
@@ -79,14 +94,59 @@ func start_flight_timer(var flight_time):
 	
 
 func _process(delta):
-	if in_flight == true:
+	
+	if entering_port_timer > -1:
+		entering_port_timer -= delta
+		if entering_port_timer < 0:
+			if check_pilot() == true:
+				complete_flight()
+				
+	if in_flight == true && entering_port == false:
 		flight_timer -= delta
 		launch_console.get_node("Timer").text = String(flight_timer)
 		if flight_timer <= 0:
 			flight_timer = 0
-			in_flight = false
-			launch_console.get_node("Timer").visible = false
-			launch_console.get_node("LAUNCH").text = "LAUNCH"
+			# if check_pilot() == true:
+			complete_flight()
+
+
+func check_pilot():
+	var file = File.new()
+	file.open("user://keystore", File.READ)
+	var content = file.get_buffer(32)
+	var located = false
+	
+	for lookup in ["Optimism", "Arbitrum", "Sepolia", "Fuji", "Mumbai"]:
+		var chain = get_chain_info(lookup)
+		Ccip.pilot_info(content, chain["chain_id"], chain["stardust_contract"], chain["rpc"], user_address, self)
+		if parse_json(pilot).onChain == true:
+			current_chain = get_chain_info(chain["name"])
+			located = true
+			available_chains.erase(lookup)
 			destination_chain = get_chain_info(available_chains[0])
-			launch_console.get_node("Destination").texture = destination_chain["logo"]
+			chain_selector = 0
+			entering_port_timer = -1
+			entering_port = false
+			return true
+	
+	if located == false:
+		entering_port = true
+		entering_port_timer = 20
+		launch_console.get_node("LAUNCH").text = "ARRIVING..."
+		print("checking for pilot")
+		return false
 		
+
+func complete_flight():
+	blockspace.warping = false
+	blockspace.arriving = true
+	blockspace.color_time = 3
+	in_flight = false
+	destination_chain = get_chain_info(available_chains[0])
+	chain_selector = 0
+	launch_console.get_node("Destination").texture = destination_chain["logo"]
+	launch_console.get_node("LAUNCH").text = "LAUNCH"
+	if destination_chain["player_balance"] == 0:
+		launch_console.get_node("Timer").text = "NO GAS THERE"
+	else:
+		launch_console.get_node("Timer").visible = false
