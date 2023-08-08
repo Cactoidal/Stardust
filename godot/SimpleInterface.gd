90723be92cf6cd00807b100a3d2acd629e5bb398
Cactus <115384394+Cactoidal@users.noreply.github.com>
2023-08-08 03:46:56 -0400

    Update SimpleInterface.gd

--- godot/SimpleInterface.gd
+++ godot/SimpleInterface.gd
@@ -1,4 +1,13 @@
 extends Control
 
+#choose destination
+#not visible in transit
+
 func _ready():
 	Global.launch_console = self
+	$Destination.texture = Global.destination_chain["logo"]
+
+	if Global.destination_chain["player_balance"] == 0:
+		$Timer.text = "NO GAS THERE"
+		$Timer.visible = true
+
