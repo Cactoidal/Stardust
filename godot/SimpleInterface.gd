8d913f7b12c4aaf88532f0f2e7644d404f718e1c
Cactus <115384394+Cactoidal@users.noreply.github.com>
2023-08-08 21:35:13 -0400

    Update SimpleInterface.gd

--- godot/SimpleInterface.gd
+++ godot/SimpleInterface.gd
@@ -1,10 +1,15 @@
 extends Control
 
+
+
 func _ready():
 	Global.launch_console = self
-	$Destination.texture = Global.destination_chain["logo"]
+	$Destination.texture = Global.get_chain_info(Global.destination_chain)["logo"]
 
-	if Global.destination_chain["player_balance"] == 0:
+	if Global.get_chain_info(Global.destination_chain)["player_balance"] == 0:
 		$Timer.text = "NO GAS THERE"
 		$Timer.visible = true
 
+
+func set_pilot():
+	pass
